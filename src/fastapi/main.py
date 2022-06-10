9fe5521ef76adac6098a8da16e006459e1451cc5
Grant Birkinbine <grantbirki@github.com>
2022-06-09 22:30:36 -0600

    fmt

--- src/fastapi/main.py
+++ src/fastapi/main.py
@@ -1,13 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
 
 @app.get("/api/cache")
-def read_root():
-    return {"Hello": "World"}
+async def cache(payload: str = False):
+    if not payload:
+        raise HTTPException(status_code=400, detail="payload query parameter is required")
+
+    # NOTE: Here we can interact with the payload which will be a graphql query
+    # We can do sorting, inspection, etc
+    # For now, we just want cloudflare to cache the response so we return a 200
+
+    return "OK"
 
 
 @app.get("/health")
-def read_root():
+async def health():
     return "OK"
