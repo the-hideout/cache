f8069574d10d20c05577c6f6ff2ece7bc384ecdd
Grant Birkinbine <grantbirki@github.com>
2022-08-09 23:21:44 -0600

    redis testing

--- src/cache/main.go
+++ src/cache/main.go
@@ -4,12 +4,16 @@ import (
 	"fmt"
 	"net/http"
 
+	"context"
 	"encoding/json"
 	"io/ioutil"
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v9"
 )
+
+var ctx = context.Background()
 
 func config() map[string]interface{} {
 	// Read config file
@@ -36,7 +40,26 @@ func main() {
 
 	// Load the config file
 	config := config()
-	fmt.Printf("%+v\n", config)
+
+	// Create a new redis client
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     fmt.Sprintf("%s:%.0f", config["redis_host"], config["redis_port"]),
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	// test 1
+	err := rdb.Set(ctx, "key", "value", 0).Err()
+	if err != nil {
+		panic(err)
+	}
+
+	// test 2
+	val, err := rdb.Get(ctx, "key").Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("key", val)
 
 	// Create a new gin router
 	r := gin.Default()
