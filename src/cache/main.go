bce77af793a1aab005acd253e6664bc14568923e
Grant Birkinbine <grantbirki@github.com>
2022-08-09 23:01:22 -0600

    config and health endpoint

--- src/cache/main.go
+++ src/cache/main.go
@@ -1,17 +1,51 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+
+	"encoding/json"
+	"io/ioutil"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
+func config() map[string]interface{} {
+	// Read config file
+	configFile, err := os.Open("config.json")
+	if err != nil {
+		os.Exit(1)
+	}
+	defer configFile.Close()
+
+	// Read the config file into bytes
+	byteValue, _ := ioutil.ReadAll(configFile)
+
+	// Define the interface to unmarshal
+	var result map[string]interface{}
+
+	// Parse the bytes into the interface (unstructured data)
+	json.Unmarshal([]byte(byteValue), &result)
+
+	// Return the interface (dict) of values
+	return result
+}
+
 func main() {
+
+	// Load the config file
+	config := config()
+	fmt.Printf("%+v\n", config)
+
+	// Create a new gin router
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
+
+	// Health endpoint
+	r.GET("/health", func(c *gin.Context) {
+		c.String(http.StatusOK, "OK")
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
+	// Start the application on 0.0.0.0:8080
+	r.Run()
 }
